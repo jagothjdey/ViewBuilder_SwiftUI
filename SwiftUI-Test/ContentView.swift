5eb4116446fc9e203f5288449fbf111c20e2faa1
dey <jagothjyotidey91@gmail.com>
2020-04-18 20:31:19 +0200

    Custom View Construction

--- SwiftUI-Test/ContentView.swift
+++ SwiftUI-Test/ContentView.swift
@@ -1,21 +1,58 @@
-//
-//  ContentView.swift
-//  SwiftUI-Test
-//
-//  Created by Jagoth Jyoti Dey on 18.04.20.
-//  Copyright © 2020 Jagoth. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        var myString: String = "Welcome to SwiftUI"
+        
+        return MyHStack{
+            Text("Text 1")
+            Text("Text 2")
+            MyVStack {
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star")
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct StandardTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .background(Color.white)
+            .border(Color.gray, width: 0.2)
+            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
+    }
+}
+
+
+struct MyHStack<Content : View>: View{
+    let content : () -> Content
+    
+    init(@ViewBuilder content : @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body : some View{
+        HStack{
+            content()
+        }
+    }
+}
+
+struct MyVStack<Content: View>: View {
+    let content: () -> Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            content()
+        }
+        .font(.largeTitle)
     }
 }
