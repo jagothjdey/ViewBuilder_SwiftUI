a9b56c0755bfd41ff0c9819cf29c4a347655691c
dey <jagothjyotidey91@gmail.com>
2020-04-18 18:10:18 +0200

    Initial Commit

---
+++ SwiftUI-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Test
+//
+//  Created by Jagoth Jyoti Dey on 18.04.20.
+//  Copyright © 2020 Jagoth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
